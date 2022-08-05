64bac2288e9ca33d3e8518becebc238c6a44c461
Muhammed Talas <clashus1234@gmail.com>
2022-08-05 09:31:04 +0300

    Sign-up function created

--- login.cpp
+++ login.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <windows.h>
 
-
 using namespace std;
+
+string username_;
+string password_;
+int securitycode_;
+
+
+
 class User{
 private:
     string username;
     string password;
     int securitycode;
 public:
+    string getPassword(){
+        return password;
+    }
     User(string Username,string Password,int Securitycode){
         username=Username;
         password=Password;
@@ -16,6 +25,19 @@ public:
     }
 };
 
+void Signup(){
+    cout << "\tSign-up Screen" << endl;
+    cout << "Enter username:";
+    cin >> username_;
+    cout << "Enter password:";
+    cin >> password_;
+    cout << "Enter security code:";
+    cin >> securitycode_;
+}
+
 int main(){
+    cout << "Start";
+    Signup();
+    User user=User(username_,password_,securitycode_);
 
 }
