845fc9ba6486c19669614cdd610b026d5840ec7a
Muhammed Talas <clashus1234@gmail.com>
2022-08-05 09:07:02 +0300

    login.cpp created and Start code written

---
+++ login.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+
+
+using namespace std;
+class User{
+private:
+    string username;
+    string password;
+    int securitycode;
+public:
+    User(string Username,string Password,int Securitycode){
+        username=Username;
+        password=Password;
+        Securitycode=securitycode;
+    }
+};
+
+int main(){
+
+}
